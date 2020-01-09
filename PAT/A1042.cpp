5e0e4a449f2f9a47dffc7aa2e61f614cda8e771f
Sakura <843522319@qq.com>
2020-01-09 22:40:14 +0800

    试验中2

--- PAT/A1042.cpp
+++ PAT/A1042.cpp
@@ -2,20 +2,54 @@
 #include <cstdio>
 using namespace std;
 
+int getIndex(int a[], int n, int findElement)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == findElement)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    // string str1[54] = {""};
-    // char shuffle[5] = {'S', 'H', 'C', 'D', 'J'};
-    // int n = 54;
-    // for(int i = 0; i < n; i++)
-    // {
-    //     str1[i] = 
-    // }
-    string str = "";
-    char a = 'A';
-    
+    int shufflePos[54] = {0}; // 卡牌位置
+    int tempPos[54] = {0};    // 临时存放要卡牌要放置的位置
+    int changePos[54] = {0};  // 映射
+    int n = 54;
+    int ind;
+    int timeN, inPos;
+    scanf("%d", &timeN);
+    for (int i = 0; i < n; i++)
+    {
+        shufflePos[i] = i;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &inPos);
+        tempPos[i] = inPos - 1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        changePos[tempPos[i]] = shufflePos[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        ind = getIndex(changePos, n, i);
+        if (ind != -1)
+        {
+            shufflePos[i] =  ind;
+        }
+        
+    }
 
-    cout << str1[0];
+    // string str = "";
+    // char a = 'A';
+
+    // printf("%c%d", a, 1);
     system("pause");
     return 0;
 }

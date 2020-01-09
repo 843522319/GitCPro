591f25070f4e35951644759f1e10f4e8c2751874
Sakura <843522319@qq.com>
2020-01-09 20:03:21 +0800

    试验中

---
+++ PAT/A1042.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    // string str1[54] = {""};
+    // char shuffle[5] = {'S', 'H', 'C', 'D', 'J'};
+    // int n = 54;
+    // for(int i = 0; i < n; i++)
+    // {
+    //     str1[i] = 
+    // }
+    string str = "";
+    char a = 'A';
+    
+
+    cout << str1[0];
+    system("pause");
+    return 0;
+}

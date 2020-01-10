9d6a200020491cb256da531f1a7abb8cd49d068b
Sakura <843522319@qq.com>
2020-01-10 16:20:41 +0800

    1024完成

--- PAT/A1042.cpp
+++ PAT/A1042.cpp
@@ -2,54 +2,69 @@
 #include <cstdio>
 using namespace std;
 
-int getIndex(int a[], int n, int findElement)
-{
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] == findElement)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
 
 int main()
 {
-    int shufflePos[54] = {0}; // 卡牌位置
-    int tempPos[54] = {0};    // 临时存放要卡牌要放置的位置
-    int changePos[54] = {0};  // 映射
+    /*
+    0-53 每个数字代表一个牌，S1,S2...S13,.....J1,J2
+    */
+    char shuffle[5] = {'S', 'H', 'C', 'D', 'J'};
+    int shufflePos[54] = {0}; // 操作前卡牌位置
+    int tempPos[54] = {0};    // 存放卡牌要放置的位置
+    int changePos[54] = {0};  // 操作后卡牌位置
     int n = 54;
     int ind;
     int timeN, inPos;
     scanf("%d", &timeN);
     for (int i = 0; i < n; i++)
     {
-        shufflePos[i] = i;
+        shufflePos[i] = i; // 初始化位置编号
     }
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &inPos);
         tempPos[i] = inPos - 1;
     }
-    for (int i = 0; i < n; i++)
+    while (timeN--)
     {
-        changePos[tempPos[i]] = shufflePos[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        ind = getIndex(changePos, n, i);
-        if (ind != -1)
+        for (int i = 0; i < n; i++)
         {
-            shufflePos[i] =  ind;
+            changePos[tempPos[i]] = shufflePos[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            shufflePos[i] = changePos[i]; // 更新卡牌位置
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(shufflePos[i] < 13)
+        {
+            printf("%c%d", shuffle[0], shufflePos[i] + 1);
+        }
+        else if(shufflePos[i] >= 13 && shufflePos[i] < 26)
+        {
+            printf("%c%d", shuffle[1], (shufflePos[i] % 13) + 1);
+        }
+        else if(shufflePos[i] >= 26 && shufflePos[i] < 39)
+        {
+            printf("%c%d", shuffle[2], (shufflePos[i] % 13) + 1);
+        }
+        else if(shufflePos[i] >= 39 && shufflePos[i] < 52)
+        {
+            printf("%c%d", shuffle[3], (shufflePos[i] % 13) + 1);
+        }
+        else if(shufflePos[i] >= 52 && shufflePos[i] < 54)
+        {
+            printf("%c%d", shuffle[4], (shufflePos[i] % 2) + 1);
         }
         
+        if(i < n-1)
+        {
+            printf(" ");
+        }
     }
-
-    // string str = "";
-    // char a = 'A';
-
-    // printf("%c%d", a, 1);
     system("pause");
     return 0;
 }

8386de36e774a7c7441f6f422b43f8aa73308371
Sakura <843522319@qq.com>
2020-01-12 16:10:53 +0800

    完成

---
+++ PAT/B1022.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    int a, b, d;
+    scanf("%d%d%d", &a, &b, &d);
+    int sum = a + b;
+    int ans[40], num = 0;
+    do
+    {
+        ans[num++] = sum % d;
+        sum = sum / d;
+    } while (sum != 0);
+    for(int i = num - 1; i >= 0; i--)
+    {
+        printf("%d", ans[i]);
+    }
+    
+    system("pause");
+    return 0;
+}
